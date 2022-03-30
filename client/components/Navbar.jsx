6c83aafd436dfdbe8d5fe1d3d8bb638b88d6a100
ramriddlez <ramankangz@outlook.com>
2022-03-30 11:58:18 -0400

    typo fix

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Narbar = () => {
+const Navbar = () => {
   return (
     <div>
         
@@ -8,4 +8,4 @@ const Narbar = () => {
   )
 }
 
-export default Narbar
+export default Navbar
