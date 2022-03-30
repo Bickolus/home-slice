0580af847dcf4109913c865fe41e155e45cd4b30
Alex C <cxz980314@gmail.com>
2022-03-30 12:44:42 -0400

    define Footer in Layout.js

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Narbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = ({children}) => {
   return (
-    <>
+    <div>
         <Narbar/>
         {children}
         <Footer/>
-    </>
+    </div>
   )
 }
 
