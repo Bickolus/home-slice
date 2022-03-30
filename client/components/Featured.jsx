d476ef41a4eb89ef9d9a0d2f43b6c8d57dd1730d
Alex C <cxz980314@gmail.com>
2022-03-30 19:31:41 -0400

    fix slick layout and autoplay function for featured pizza

--- client/components/Featured.jsx
+++ client/components/Featured.jsx
@@ -11,8 +11,38 @@ class Featured extends Component {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     return (
@@ -20,13 +50,22 @@ class Featured extends Component {
         <div className={styles.wrappera}>
         <Slider {...settings}>
           <div className={styles.imgContainer}>
-            <Image src='/img/featured.png' width='800' height='800' alt="" objectFit="fill" />
+            <Image src='/img/featured.png' width='600' height='600' alt="" objectFit="fill" />
           </div>
           <div className={styles.imgContainer}>
-          <Image src='/img/featured2.png' width='800' height='800' alt=""  objectFit="fill" />
+          <Image src='/img/featured2.png' width='600' height='600' alt=""  objectFit="fill" />
           </div>
           <div className={styles.imgContainer}>
-          <Image src='/img/featured3.png' width='800' height='800' alt=""  objectFit="fill" />
+          <Image src='/img/featured3.png' width='600' height='600' alt=""  objectFit="fill" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image src='/img/featured.png' width='600' height='600' alt="" objectFit="fill" />
+          </div>
+          <div className={styles.imgContainer}>
+          <Image src='/img/featured2.png' width='600' height='600' alt=""  objectFit="fill" />
+          </div>
+          <div className={styles.imgContainer}>
+          <Image src='/img/featured3.png' width='600' height='600' alt=""  objectFit="fill" />
           </div>
         </Slider>
         </div>
