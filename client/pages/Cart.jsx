5eb5b77427b7527f2db5d1f65db712d2389dd500
Alex C <cxz980314@gmail.com>
2022-03-31 14:08:00 -0400

    add Cart page and styling

---
+++ client/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from "../styles/Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Cart
