ed349d08d4585cc635d60bf2a83f09e9127f8f22
Alex C <cxz980314@gmail.com>
2022-03-31 13:04:44 -0400

    import PizzaList

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Featured from '../components/Featured'
+import PizzaList from '../components/PizzaList'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
