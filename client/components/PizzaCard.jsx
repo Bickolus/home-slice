6282c271702f0804d76aec07adb43a988f2c2482
Alex C <cxz980314@gmail.com>
2022-03-31 13:01:17 -0400

    adding PizzaCard

---
+++ client/components/PizzaCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PizzaCard = () => {
+  return (
+    <div>PizzaCard</div>
+  )
+}
+
+export default PizzaCard
--- client/components/PizzaList.jsx
+++ client/components/PizzaList.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import PizzaCard from './PizzaCard';
 import styles from '../styles/PizzaList.module.css'
 import Image from "next/image";
 
 const PizzaList = () => {
   return (
-    <div>PizzaList</div>
+    <div>
+        
+    </div>
   )
 }
 
