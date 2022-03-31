e00f91bf157beeab55ee8412fce043fbdfe54faf
Alex C <cxz980314@gmail.com>
2022-03-31 12:57:13 -0400

    adding pizzaList and styling

---
+++ client/components/PizzaList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/PizzaList.module.css'
+import Image from "next/image";
+
+const PizzaList = () => {
+  return (
+    <div>PizzaList</div>
+  )
+}
+
+export default PizzaList
