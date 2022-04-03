318eb552b6b07a58aab027b6fad921de45688add
ramriddlez <ramankangz@outlook.com>
2022-04-02 23:29:56 -0400

    added routes and imports

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import pizzaData from "../pizza-data";
+import { useDispatch, useSelector } from 'react-redux'
 import { Container, Row, Col } from "react-bootstrap";
+import { getAllPizzas } from '../actions/pizzaAction'
 import Pizza from "../components/Pizza";
 
 const Homepage = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {dispatch(getAllPizzas());
+  } , [dispatch]);
   return (
     <>
       <Container>
