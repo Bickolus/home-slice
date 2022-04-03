37476122c5ae92c30042906a29365916f1f24ad9
ramriddlez <ramankangz@outlook.com>
2022-04-02 21:37:43 -0400

    added react/redux and imports

--- server/seeder.js
+++ server/seeder.js
@@ -4,3 +4,25 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/connection");
 const Pizza = require("./models/Pizza.js");
 const Pizzas = require("../client/src/pizza-data");
+
+//config dot env and mongodb conn file
+dotenv.config();
+connectDB();
+
+//import data
+const importData = async () => {
+    try {
+        await Pizza.deleteMany();
+        const sampleData = Pizzas.map((pizza) => {
+            return { ...pizza };
+        });
+        await Pizza.insertMany(sampleData);
+        console.log("DATA IMPOrted");
+        process.exit();
+    } catch (error) {
+        console.log(`${error}`);
+        process.exit(1);
+    }
+};
+
+importData();
